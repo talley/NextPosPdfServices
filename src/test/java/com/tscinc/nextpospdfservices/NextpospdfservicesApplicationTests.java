0406489a053e63e3e94cab7d53376e421664af4e
Talley Ouro Work <talley.ouro@ncagr.gov>
2023-12-11 13:39:06 -0500

    Added basic testing to InvoiceManager class

--- src/test/java/com/tscinc/nextpospdfservices/NextpospdfservicesApplicationTests.java
+++ src/test/java/com/tscinc/nextpospdfservices/NextpospdfservicesApplicationTests.java
@@ -1,13 +1,38 @@
 package com.tscinc.nextpospdfservices;
 
+import com.tscinc.nextpospdfservices.helpers.InvoiceManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.io.File;
 
 @SpringBootTest
 class NextpospdfservicesApplicationTests {
 
+	private InvoiceManager invoiceManager=new InvoiceManager();
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void can_generate_pdf(){
+
+		Exception exception =new Exception();
+		String pdf="C:\\temp\\invoice1.pdf";
+		File mypdffile=new File(pdf);
+		if(mypdffile.exists()){
+			mypdffile.delete();
+		}
+		try
+		{
+			invoiceManager.createPDF(pdf);
+		}
+		catch (Exception ex){
+			ex.printStackTrace();
+			ex=exception;
+		}
+
+		Assert.isNull(exception,"Can   create PDF file");
+	}
 }
